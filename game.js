a0578bd48d3e6fb1d057f9e7e7c948804fcef500
John-Jelatis <47511684+John-Jelatis@users.noreply.github.com>
2019-03-03 18:25:51 -0600

    fucking work this time, ok?

--- game.js
+++ game.js
@@ -1,15 +1,18 @@
 // Error Handeler
 window.onerror = function() { alert(Array.from(arguments)) };
 // Production?
-const production = true;
+const VERSION = {
+	"PRODUCTION": !0,
+	"VERSION"	: "1.0"
+};
 // Spritesheet thing
 function Spritesheet(data) {
 	this.data = data;
 };
 Spritesheet.prototype.draw = function(c, s, x, y, w, h) {
-	if(!production) {
+	if(!VERSION.PRODUCTION) {
 		c.strokeStyle	= 'black';
-		c.lineWidth		= 2;
+		c.lineWidth		= 0.025;
 		c.strokeRect(x, y, w, h);
 	};
 	if(s in this.data)
@@ -18,22 +21,34 @@ Spritesheet.prototype.draw = function(c, s, x, y, w, h) {
 // Entities are useful
 function Entity(texture, x, y, w, h) {
 	this.t = texture;
-	this.x = x-900;
-	this.vX = 0.37;
+	this.x = x;
+	this.vX = 0;
 	this.y = y;
-	this.vY = -0.35;
+	this.vY = -0.2;
 	this.w = w;
 	this.h = h;
+	this.r = 0;
 };
 Entity.prototype.draw = function(sprite, context) {
-	sprite.draw(context, this.t, this.x, this.y, this.w, this.h);
+	context.save();
+	context.translate(this.x + this.w / 2, this.y + this.h / 2);
+	context.rotate(this.r);
+	sprite.draw(context, this.t, -this.w / 2, -this.h / 2, this.w, this.h);
+	context.restore();
 };
 Entity.prototype.tick = function(ms) {
 	this.x += this.vX * ms;
 	this.y += this.vY * ms;
-	this.vY += ms / 3500;
-	if(this.y > 210) this.vY = 0;
-	if(this.vY == 0) this.vX *= 0.99;
+	this.vY += ms / 7000;
+	if(this.x > 90) this.r = Math.PI / 8;
+	if(this.x > 98) this.r = Math.PI / 5;
+	if(this.x > 100) this.r = Math.PI / 3;
+	if(this.x > 101) this.r = Math.PI / 2;
+	if(this.y + this.h > 75 && this.x < 100) {
+		this.vY	= 0;
+		this.y	= 75 - this.h;
+		// this.vX	*= 0.99;
+	}
 };
 // Make a Game class
 function Game(sprites) {
@@ -49,19 +64,20 @@ function Game(sprites) {
 	this.context	= this.canvas.getContext('2d');
 	this.sprites	= new Spritesheet(sprites);
 	// Other things
-	this.canvas.style.imageRendering = 'pixelated';
-	this.canvas.width	= 960;
-	this.canvas.height	= 540;
-	this.x = 0;
+	this.canvas.width	= window.innerWidth;
+	this.canvas.height	= window.innerHeight;
+	this.canvas.style.imageRendering	= 'pixelated';
+	this.context.imageSmoothingEnabled	= false;
 };
 Game.prototype.init = function() {
 	this.entities	= [
-		new Entity('paul-mccartney', -128, -64, 96, 192),
-		new Entity('george-harrison', -256, -128, 96, 192),
-		new Entity('john-lennon', -384, -192, 96, 192)
+		new Entity('paul-mccartney', 45, -64, 20, 38),
+		new Entity('george-harrison', 20, -128, 20, 38),
+		new Entity('john-lennon', -50, -192, 20, 38)
 	];
+	this.entities[2].vX = 0.01;
 	this.levels		= [];
-	this.player		= new Entity('ringo-starr', 0, 0, 96, 192);
+	this.player		= new Entity('ringo-starr', 70, 0, 20, 38);
 	this.running = true;
 	this.lastTick = new Date().getTime();
 	document.body.appendChild(this.canvas);
@@ -77,11 +93,14 @@ Game.prototype.tick	= function() {
 };
 Game.prototype.draw	= function() {
 	this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	this.context.save();
+	this.context.scale(this.canvas.height / 100, this.canvas.height / 100);
 	this.context.fillStyle = '#0F9';
-	this.context.fillRect(0, this.canvas.height * 0.75, this.canvas.width, this.canvas.height * 0.25);
+	this.context.fillRect(0, 75, 100, 25);
 	for(var e = 0; e < this.entities.length; e++)
 		this.entities[e].draw(this.sprites, this.context);
 	this.player.draw(this.sprites, this.context);
+	this.context.restore();
 };
 // Intentionally make game public
 var game = null;
@@ -98,20 +117,20 @@ window.addEventListener('DOMContentLoaded', function(evt) {
 			'x'		: 0,
 			'y'		: 0,
 			'w'		: 25,
-			'h'		: 46
+			'h'		: 45
 		},
 		'paul-mccartney': {
 			'img'	: '-beatles-abbey-road',
 			'x'		: 30,
 			'y'		: 0,
 			'w'		: 22,
-			'h'		: 46
+			'h'		: 45
 		},
 		'ringo-starr': {
 			'img'	: '-beatles-abbey-road',
-			'x'		: 54,
-			'y'		: 2,
-			'w'		: 25,
+			'x'		: 55,
+			'y'		: 1,
+			'w'		: 24,
 			'h'		: 44
 		},
 		'john-lennon': {
@@ -119,7 +138,7 @@ window.addEventListener('DOMContentLoaded', function(evt) {
 			'x'		: 81,
 			'y'		: 0,
 			'w'		: 25,
-			'h'		: 46
+			'h'		: 45
 		}
 	});
 });
